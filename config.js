74bccd82a9a20691211ef6940d5385df8f621a26
2410080052-alt <2410080052@klh.edu.in>
2026-05-01 11:20:34 +0530

    Initialize Supabase client in config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://wbplwkgwtjzsxhthqqqq.supabase.co';
+const supabaseKey = 'sb_publishable_UFwgd2pQnLA_sJfOvBu-pQ_EArPdwEw';
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
